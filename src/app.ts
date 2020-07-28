144bdb1c47fef193322964cede250ce6fe0c3752
Si Thu San <sithusanss@gmail.com>
2020-07-28 17:16:03 +0630

    Added Config

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,9 @@ import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
 import { createConnection, Connection } from "typeorm";
 import * as firebase from "firebase/app";
+import firebaseConfig from "./config/firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD6Xw4-Jy6QeCb2d_kfRstEHJFbNa6ijJ4",
-  authDomain: "auth-5096a.firebaseapp.com",
-  databaseURL: "https://auth-5096a.firebaseio.com",
-  projectId: "auth-5096a",
-  storageBucket: "auth-5096a.appspot.com",
-  messagingSenderId: "717541276022",
-  appId: "1:717541276022:web:6efaa346c65f4174afa257",
-  measurementId: "G-9LHXGQDTPR",
-};
+
 
 async function bootstrap() {
   const schema = await buildSchema({
